2b09a4a3a59fa94f5996bd1f4ed408e4d537eb98
danielrakaczki <daniel.rakaczki@gmail.com>
2025-03-16 20:06:27 +0100

    chore: contact section fixes

--- app/components/sections/contact.tsx
+++ app/components/sections/contact.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { Button } from "../button";
 
 type ContactSectionProps = {
@@ -9,7 +9,7 @@ type ContactSectionProps = {
 
 export const ContactSection = ({ title, description, image }: ContactSectionProps) => {
   return (
-    <section id="contact1" className="mt-48 grid w-full grid-cols-1 md:mt-16 lg:mt-24 lg:max-w-screen-2xl">
+    <section id="contact1" className="mx-auto mt-48 grid w-full grid-cols-1 md:mt-16 lg:mt-24 lg:max-w-screen-2xl">
       <div className="from-primary to-primary-2 relative flex flex-col-reverse items-center gap-8 rounded-[1em] bg-gradient-to-br p-8 text-xl text-white md:flex-row">
         <div className="flex flex-col gap-4 md:w-[70%]">
           <h2 className="text-2xl font-bold text-balance lg:text-3xl">{title}</h2>
