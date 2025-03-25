191d42f8ac37d38b68ccdf04bcd2346bbe197600
danielrakaczki <daniel.rakaczki@gmail.com>
2025-03-25 22:42:57 +0100

    chore: button component got ref prop

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/app/utils";
+import { Ref } from "react";
 
 type ButtonProps = {
+  ref?: Ref<HTMLButtonElement>;
   color: "primary" | "tertiary" | "white";
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
