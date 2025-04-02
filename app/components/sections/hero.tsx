a788cb80ab96e4fd7d24b56b70fb3f1b56179cce
danielrakaczki <daniel.rakaczki@gmail.com>
2025-04-02 21:19:23 +0200

    eat: inview animations added to <HeroSection>

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -1,29 +1,50 @@
 import HeroImage from "@/public/hero-image.png";
 import Image from "next/image";
 import Link from "next/link";
+import { Motion } from "../motion/motion";
+import { WHILE_IN_VIEW_VARIANTS } from "../motion/animations";
 
 export const HeroSection = () => {
   return (
     <section id="hero" className="w-full">
       <div className="mx-auto grid w-full gap-4 p-8 md:grid-cols-2 md:place-items-stretch lg:max-w-screen-2xl">
-        <div className="flex flex-col justify-center gap-8 text-balance">
-          <h1 className="text-3xl leading-tight font-bold lg:text-[clamp(3rem,3.33vw,4rem)]">
+        <Motion.div
+          variants={WHILE_IN_VIEW_VARIANTS}
+          whileInView="shown"
+          initial="hidden"
+          className="flex flex-col justify-center gap-8 text-balance"
+        >
+          <Motion.h1 variants={WHILE_IN_VIEW_VARIANTS} className="text-3xl leading-tight font-bold lg:text-[clamp(3rem,3.33vw,4rem)]">
             <span className="text-primary">Biztonságos</span> és <span className="text-primary">profi</span> villanyszerelés lakossági
             ügyfeleknek!
-          </h1>
-          <p className="text-tertiary font-semibold lg:text-xl">
+          </Motion.h1>
+          <Motion.p variants={WHILE_IN_VIEW_VARIANTS} className="text-tertiary font-semibold lg:text-xl">
             Regisztrált villanyszerelőként biztosítjuk, hogy otthonod elektromos rendszere biztonságos, szabványos és megbízható legyen.
-          </p>
-          <div className="mt-4 flex flex-wrap items-stretch justify-start gap-4 text-lg md:text-xl lg:text-2xl">
-            <Link className="btn primary" href="tel:+36121231234">
-              Hívjon most
-            </Link>
-            <Link href="/#services" className="btn tertiary">
-              Szolgáltatások
-            </Link>
-          </div>
-        </div>
-        <div className="relative aspect-square max-md:row-start-1" aria-disabled>
+          </Motion.p>
+          <Motion.div
+            key="buttons"
+            variants={WHILE_IN_VIEW_VARIANTS}
+            className="mt-4 flex flex-wrap items-stretch justify-start gap-4 text-lg md:text-xl lg:text-2xl"
+          >
+            <Motion.div key="primary" variants={WHILE_IN_VIEW_VARIANTS}>
+              <Link className="btn primary" href="tel:+36121231234">
+                Hívjon most
+              </Link>
+            </Motion.div>
+            <Motion.div key="tertiary" variants={WHILE_IN_VIEW_VARIANTS}>
+              <Link href="/#services" className="btn tertiary">
+                Szolgáltatások
+              </Link>
+            </Motion.div>
+          </Motion.div>
+        </Motion.div>
+        <Motion.div
+          variants={WHILE_IN_VIEW_VARIANTS}
+          whileInView="shown"
+          initial="hidden"
+          className="relative aspect-square max-md:row-start-1"
+          aria-disabled
+        >
           <Image
             fill
             src={HeroImage}
@@ -32,7 +53,7 @@ export const HeroSection = () => {
             objectPosition="center"
             placeholder="empty"
           />
-        </div>
+        </Motion.div>
       </div>
     </section>
   );
