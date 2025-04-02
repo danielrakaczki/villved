d7d0f74a394615c67a5184ca96b3d1760d7da771
danielrakaczki <daniel.rakaczki@gmail.com>
2025-04-02 21:39:57 +0200

    feat: <AboutUsSection> got animations

--- app/components/sections/about-us.tsx
+++ app/components/sections/about-us.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
-import TeamImage from "@/public/team.png";
 import TeamWorkingImage from "@/public/team-working.png";
-import { SectionHeader } from "../section-header";
+import TeamImage from "@/public/team.png";
+import Image from "next/image";
+import { WHILE_IN_VIEW_VARIANTS } from "../motion/animations";
+import { Motion } from "../motion/motion";
 import { Section } from "../section";
+import { SectionHeader } from "../section-header";
 
 export const AboutUsSection = () => {
   return (
     <Section id="about-us">
       <SectionHeader>Rólunk</SectionHeader>
-      <div className="flex flex-col gap-8 md:flex-row">
-        <div className="relative aspect-video md:flex-1">
+      <Motion.div variants={WHILE_IN_VIEW_VARIANTS} initial="hidden" whileInView="shown" className="mb-8 flex flex-col gap-8 md:flex-row">
+        <Motion.div variants={WHILE_IN_VIEW_VARIANTS} className="relative aspect-video md:flex-1">
           <Image
             src={TeamImage}
             fill
@@ -19,26 +21,30 @@ export const AboutUsSection = () => {
             alt="A csapat"
             className="rounded-[1em]"
           />
-        </div>
-        <div className="flex flex-col gap-8 md:flex-1">
-          <div>
-            <h3 className="mb-4 text-2xl font-bold">Kik vagyunk mi?</h3>
-            <p>
+        </Motion.div>
+        <Motion.div variants={WHILE_IN_VIEW_VARIANTS} className="flex flex-col gap-8 md:flex-1">
+          <Motion.div variants={WHILE_IN_VIEW_VARIANTS}>
+            <Motion.h3 variants={WHILE_IN_VIEW_VARIANTS} className="mb-4 text-2xl font-bold">
+              Kik vagyunk mi?
+            </Motion.h3>
+            <Motion.p variants={WHILE_IN_VIEW_VARIANTS}>
               Lorem ipsum dolor sit amet consectetur. Lectus tristique dignissim egestas risus arcu ornare sit ipsum. Integer magna
               facilisis senectus a integer. Id mollis a pulvinar suspendisse maecenas aliquet.
-            </p>
-          </div>
-          <div>
-            <h3 className="mb-4 text-2xl font-bold">Mik a kompetenciáink?</h3>
-            <p>
+            </Motion.p>
+          </Motion.div>
+          <Motion.div variants={WHILE_IN_VIEW_VARIANTS}>
+            <Motion.h3 variants={WHILE_IN_VIEW_VARIANTS} className="mb-4 text-2xl font-bold">
+              Mik a kompetenciáink?
+            </Motion.h3>
+            <Motion.p variants={WHILE_IN_VIEW_VARIANTS}>
               Lorem ipsum dolor sit amet consectetur. Lectus tristique dignissim egestas risus arcu ornare sit ipsum. Integer magna
               facilisis senectus a integer. Id mollis a pulvinar suspendisse maecenas aliquet.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col gap-8 md:flex-row-reverse">
-        <div className="relative aspect-video md:flex-1">
+            </Motion.p>
+          </Motion.div>
+        </Motion.div>
+      </Motion.div>
+      <Motion.div variants={WHILE_IN_VIEW_VARIANTS} className="flex flex-col gap-8 md:flex-row-reverse">
+        <Motion.div variants={WHILE_IN_VIEW_VARIANTS} className="relative aspect-video md:flex-1">
           <Image
             src={TeamWorkingImage}
             fill
@@ -48,15 +54,17 @@ export const AboutUsSection = () => {
             alt="A csapat munka közben"
             className="rounded-[1em]"
           />
-        </div>
-        <div className="md:flex-1">
-          <h3 className="mb-4 text-2xl font-bold">Mik a célunk?</h3>
-          <p className="leading-tight">
+        </Motion.div>
+        <Motion.div variants={WHILE_IN_VIEW_VARIANTS} className="md:flex-1">
+          <Motion.h3 variants={WHILE_IN_VIEW_VARIANTS} className="mb-4 text-2xl font-bold">
+            Mik a célunk?
+          </Motion.h3>
+          <Motion.p variants={WHILE_IN_VIEW_VARIANTS} className="leading-tight">
             Lorem ipsum dolor sit amet consectetur. Lectus tristique dignissim egestas risus arcu ornare sit ipsum. Integer magna facilisis
             senectus a integer. Id mollis a pulvinar suspendisse maecenas aliquet.
-          </p>
-        </div>
-      </div>
+          </Motion.p>
+        </Motion.div>
+      </Motion.div>
     </Section>
   );
 };
