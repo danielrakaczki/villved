c43f118edcf0fcffd8b1cb5ecfb259778d94274b
danielrakaczki <daniel.rakaczki@gmail.com>
2025-04-12 22:25:30 +0200

    wip chore: removed aria hidden from navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -4,10 +4,7 @@ import { NavbarMobile } from "./navbar-mobile";
 export const Navbar = () => {
   return (
     <div className="lg:bg-background-30 z-40 text-lg lg:sticky lg:top-0 lg:backdrop-blur-lg">
-      <nav
-        className="max-lg:hidden lg:mx-auto lg:flex lg:w-full lg:max-w-screen-2xl lg:items-stretch lg:justify-end lg:gap-4 lg:px-8 lg:py-4"
-        aria-hidden
-      >
+      <nav className="max-lg:hidden lg:mx-auto lg:flex lg:w-full lg:max-w-screen-2xl lg:items-stretch lg:justify-end lg:gap-4 lg:px-8 lg:py-4">
         <NavLink href="#services">Szolgáltatásaink</NavLink>
         <NavLink href="#references">Referenciák</NavLink>
         <NavLink href="#about-us">Rólunk</NavLink>
